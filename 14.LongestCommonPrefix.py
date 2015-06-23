4d43dd605191bf48f0efa697c5d7a358ca3fb8ba
amow <freeminder@qq.com>
2015-06-23 18:16:42 +0800

    no.14

--- 14.LongestCommonPrefix.py
+++ 14.LongestCommonPrefix.py
@@ -1,1 +1,24 @@
 __author__ = 'amow'
+class Solution:
+    # @return a string
+    def find_pre(self, str1, str2):
+        for i in range(0, min(len(str1), len(str2))):
+            if str1[i] != str2[i]:
+                if i:
+                    return str1[:i]
+                else:
+                    return ""
+        return str1[:min(len(str1), len(str2))]
+
+    def longestCommonPrefix(self, strs):
+        if not strs:
+            return ""
+        pref = 0
+        for s in strs:
+            if pref == 0:
+                pref = s
+                continue
+            pref = self.find_pre(pref, s)
+            if not pref:
+                return ""
+        return pref
